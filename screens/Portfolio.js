20cc7ee552ad1973eb95b61d73625fbeee8d5a41
Feyzi KAZIK <feyzikazik@harran.edu.tr>
2021-11-26 11:10:41 +0300

    portfolio screen updated

--- screens/Portfolio.js
+++ screens/Portfolio.js
@@ -13,8 +13,11 @@ import { useFocusEffect } from "@react-navigation/native"
 import { getHoldings, getCoinMarket } from "../stores/market/marketActions";
 
 import { SIZES, COLORS, FONTS, dummyData, icons } from "../constants"
+import { BalanceInfo, IconTextButton, Chart } from '../components';
 
 const Portfolio = ({ getHoldings, myHoldings }) => {
+    
+    const [selectedCoin, setSelectedCoin] = React.useState(null)
 
     useFocusEffect(
         React.useCallback(() => {
@@ -33,11 +36,24 @@ const Portfolio = ({ getHoldings, myHoldings }) => {
                     paddingHorizontal: SIZES.padding,
                     borderBottomLeftRadius: 25,
                     borderBottomRightRadius: 25,
-                    backgroundColor: COLORS.white
+                    backgroundColor: COLORS.lightGray
                 }}
             >
                 <Text style={{ marginTop: 50, color: COLORS.white, ...FONTS.largeTitle }}>Portfolio</Text>
+            
+            <BalanceInfo
+                title= "Current Balance"
+                displayAmount={totalWallet}
+                changePct={percChange}
+                containerStyle={{
+                    marginTop: SIZES.radius,
+                    marginBottom: SIZES.padding
+                }}
+            />
+
             </View>
+
+            
         )
     }
 
@@ -50,13 +66,136 @@ const Portfolio = ({ getHoldings, myHoldings }) => {
                 }}
             >
                 {/* Header - Current balance */}
-                {renderCurrentBalanceSection}
+                {renderCurrentBalanceSection()}
 
                 {/* Chart */}
+                <Chart
+                    containerStyle={{
+                        marginTop: SIZES.radius
+                    }}
+                    chartPrices={selectedCoin ? selectedCoin?.sparkline_in_7d?.value : myHoldings[0]?.sparkline_in_7d?.value}
+                />
                 
 
                 {/* Your Assets  */}
+                <FlatList
+                    data={myHoldings}
+                    keyExtractor={item => item.id}
+                    contentContainerStyle={{
+                        marginTop: SIZES.padding,
+                        paddingHorizontal: SIZES.padding
+                    }}
+                    ListHeaderComponent={
+                        <View>
+                            {/* Section Title */}
+                            <Text style={{ ...FONTS.h2, color: COLORS.white}}>Your Assets</Text>
+                            
+                            {/* Header Label */}
+                            <View 
+                                style={{
+                                    flexDirection: 'row',
+                                    marginTop: SIZES.radius
+                            }}>
+                                <Text style={{ flex: 1, color: COLORS.lightGray3}}>Asset</Text>
+                                <Text style={{ flex: 1, color: COLORS.lightGray3, textAlign: 'right' }}>Price</Text>
+                                <Text style={{ flex: 1, color: COLORS.lightGray3, textAlign: 'right' }}>Holdings</Text>
+                            </View>
+                        </View>
+                    }
+                    renderItem={({item}) => {
+                        let priceColor = item.price_change_percentage_7d_in_currency == 0 ? COLORS.lightGray3 : (item.price_change_percentage_7d_in_currency> 0) ? COLORS.lightGreen : COLORS.red
 
+                        return (
+                            <TouchableOpacity
+                                style={{
+                                    flexDirection: 'row',
+                                    height: 55
+                                }}
+                                onPress={() => setSelectedCoin(item)}
+                            >
+                                    {/* Asset */}
+                                    <View
+                                        style={{
+                                            flex: 1,
+                                            flexDirection: 'row',
+                                            alignItems: 'center',
+                                        }}
+                                    >
+                                        <Image
+                                            source={{ uri: item.image }}
+                                            style={{
+                                                width: 20,
+                                                height: 20
+                                            }}
+                                        />
+
+                                        <Text style={{ marginLeft: SIZES.radius, color: COLORS.white, ...FONTS.h4 }}>{item.name}</Text>
+
+                                    </View>
+                                    {/* Price */}
+                                    <View
+                                        style={{
+                                            flex: 1,
+                                            justifyContent: 'center'
+                                        }}
+                                    >
+                                        <Text style={{ textAlign: 'right', color: COLORS.white, ...FONTS.h4 , lineHeight: 15 }}>{item.current_price.toLocaleString()}</Text>
+
+                                        <View
+                                            style={{
+                                                flexDirection: 'row',
+                                                alignItems: 'center',
+                                                justifyContent: 'flex-end'
+                                            }}
+                                        >
+                                            {
+                                                item.price_change_percentage_7d_in_currency != 0 &&
+                                                <Image source={icons.upArrow}
+                                                style={{
+                                                    height: 10,
+                                                    width: 10,
+                                                    tintColor: priceColor,
+                                                    transform: (item.price_change_percentage_7d_in_currency >  0) ? [{ rotate: '45deg' }] : [{ rotate: '125deg'}]
+                                                }}
+                                                />
+                                            }
+
+                                            <Text style={{ marginLeft: 5, color: priceColor, ...FONTS.body5}}>{item.price_change_percentage_7d_in_currency.toFixed(2)} %</Text>
+                                        </View>
+                                    </View>
+                                    {/* Holdings */}
+                                    <View style={{
+                                        flex: 1,
+                                        justifyContent: 'center'
+                                    }}>
+                                        <Text
+                                            style={{
+                                                textAlign: 'right',
+                                                color: COLORS.white,
+                                                ...FONTS.h4,
+                                                lineHeight: 15
+                                            }}
+                                        >
+                                            $ {item.total.toLocaleString()}
+                                        </Text>
+
+                                        <Text
+                                            style={{
+                                                textAlign: 'right',
+                                                color: COLORS.lightGray3,
+                                                ...FONTS.body5,
+                                                lineHeight: 15
+                                            }}
+                                        >
+                                            {item.qty} {item.symbol.toUpperCase()}
+                                        </Text>
+                                    </View>
+
+
+                            </TouchableOpacity>
+                        )
+                    }}
+                />
             </View>
         </MainLayout>
     )
