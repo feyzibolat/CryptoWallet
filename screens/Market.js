c6b228b69bdc6116b213f2121b2edf00c9388d4d
Feyzi KAZIK <feyzikazik@harran.edu.tr>
2021-11-29 10:12:13 +0300

    v29112021-1012 market updated

--- screens/Market.js
+++ screens/Market.js
@@ -8,6 +8,7 @@ import {
     Image,
 } from 'react-native';
 
+import { LineChart } from 'react-native-chart-kit';
 import { connect } from 'react-redux';
 import { getCoinMarket } from '../stores/market/marketActions';
 
@@ -62,6 +63,7 @@ const Tabs = () => {
 }
 const Market = ({ getCoinMarket, coins }) => {
 
+    const scrollX = React.useRef(new Animated.Value(0)).current;
     React.useEffect(() => {
         getCoinMarket()
     }, [])
@@ -111,6 +113,118 @@ const Market = ({ getCoinMarket, coins }) => {
         )
     }
 
+    function renderList() {
+        return (
+            <Animated.FlatList
+                data={marketTabs}
+                contentContainerStyle={{
+                    marginTop: SIZES.padding
+                }}
+                horizontal
+                pagingEnabled
+                scrollEventThrottle={16}
+                snapToAlignment="center"
+                showsHorizontalScrollIndicator={false}
+                keyExtractor={item => item.id}
+                onScroll={
+                    Animated.event([
+                        { nativeEvent: { contentOffset: { x: scrollX } } }
+                    ], {
+                        useNativeDriver: false
+                    })
+                }
+                renderItem={({ item, index }) => {
+                    return (
+                        <View
+                            style={{
+                                flex: 1,
+                                width: SIZES.width
+                            }}
+                        >
+                            <FlatList
+                                data={coins}
+                                keyExtractor={item => item.id}
+                                renderItem={({ item, index }) => {
+
+                                    let priceColor = (item.price_change_percentage_7d_in_currency == 0 ? COLORS.lightGray3 : (item.price_change_percentage_7d_in_currency > 0) ? COLORS.lightGreen : COLORS.red)
+
+                                    return (
+                                        <View
+                                            style={{
+                                                flexDirection: 'row',
+                                                paddingHorizontal: SIZES.padding,
+                                                marginBottom: SIZES.radius
+                                            }}
+                                        >
+                                            {/* Coins */}
+                                            <View
+                                                style={{
+                                                    flex: 1.5,
+                                                    flexDirection: 'row',
+                                                    alignItems: 'center',
+                                                }}
+                                            >
+                                                <Image
+                                                    source={{ uri: item.image }}
+                                                    style={{
+                                                        height: 20,
+                                                        width: 20
+                                                    }}
+                                                />
+
+                                                <Text style={{ marginLeft: SIZES.radius, color: COLORS.white, ...FONTS.h3 }}>{item.name}</Text>
+                                            </View>
+                                            {/* Line Chart */}
+                                            <LineChart
+                                                withVerticalLabels={false}
+                                                withHorizontalLabels={false}
+                                                withDots={false}
+                                                withInnerLines={false}
+                                                withVerticalLines={false}
+                                                withOuterLines={false}
+                                                data={{
+                                                    datasets: [
+                                                        {
+                                                            data: item.sparkline_in_7d.price
+                                                        }
+                                                    ]
+                                                }}
+                                                width={100}
+                                                height={60}
+                                                chartConfig={{
+                                                    color: () => priceColor,
+                                                    strokeWidth: 1.5
+                                                }}
+                                                bezier
+                                                style={{
+                                                    paddingRight: 0
+                                                }}
+                                            />
+
+                                            {/* Figures */}
+                                            <View
+                                                style={{
+                                                    flex: 1,
+                                                    alignItems: 'flex-end',
+                                                    justifyContent: 'center'
+
+                                                }}
+                                            >
+                                                <Text style={{
+                                                    color: COLORS.white,
+                                                    ...FONTS.h4
+                                                }}>$ {item.current_price}</Text>
+                                            </View>
+                                        </View>
+                                    )
+                                }}
+                            />
+                        </View>
+                    )
+                }}
+            />
+        )
+    }
     return (
         <MainLayout>
             <View
@@ -131,7 +245,7 @@ const Market = ({ getCoinMarket, coins }) => {
                 {renderButtons()}
 
                 {/* Market List */}
-
+                {renderList()}
 
             </View>
         </MainLayout>
