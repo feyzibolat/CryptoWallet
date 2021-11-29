6320803665d54162b700d8e00424fbc7266d8210
Feyzi KAZIK <feyzikazik@harran.edu.tr>
2021-11-29 11:47:42 +0300

    v29112021-1147-market screen updated

--- screens/Market.js
+++ screens/Market.js
@@ -22,13 +22,66 @@ const marketTabs = constants.marketTabs.map((marketTab) => ({
     ref: React.createRef()
 }))
 
-const Tabs = () => {
+const TabIndicator = ({measureLayout, scrollX}) => {
+    
+    const inputRange = marketTabs.map((_, i) => i * SIZES.width)
+
+    const translateX = scrollX.interpolate({
+        inputRange,
+        outputRange: measureLayout.map(measure => measure.x)
+    })
+    
+    return (
+        <Animated.View 
+            style={{
+                position: 'absolute',
+                left: 0,
+                height: "100%",
+                width: (SIZES.width - (SIZES.radius * 2)) / 2,
+                borderRadius: SIZES.radius,
+                backgroundColor: COLORS.lightGray,
+                transform: [{
+                    translateX
+                }]
+            }}
+        />
+    )
+}
+
+const Tabs = ({scrollX, onMarketTabPress}) => {
+
+    const [measureLayout, setMeasureLayout] = React.useState([]);
+    const containerRef = React.useRef()
+
+    React.useEffect(() => {
+        let ml = []
+        
+        marketTabs.forEach(marketTab => {
+            marketTab?.ref?.current?.measureLayout(
+                containerRef.current,
+                (x,y,width,height) => {
+                    ml.push({
+                        x,y,width, height
+                    })
+
+                    if (ml.height === marketTabs.length) {
+                        setMeasureLayout(ml)
+                    }
+                }
+            )
+        })
+    }, [containerRef.current])
+
     return (
         <View
+            ref={containerRef}
             style={{
                 flexDirection: 'row',
             }}
         >
+            {/* Tab Indicator */}
+            {measureLayout.length > 0 && <TabIndicator measureLayout={measureLayout} scrollX={scrollX} /> }
+
             {/* Tabs */}
             {marketTabs.map((item, index) => {
                 return (
@@ -37,7 +90,7 @@ const Tabs = () => {
                         style={{
                             flex: 1
                         }}
-                    //onPress
+                    onPress={() => onMarketTabPress(index)}
                     >
                         <View
                             ref={item.ref}
@@ -64,6 +117,14 @@ const Tabs = () => {
 const Market = ({ getCoinMarket, coins }) => {
 
     const scrollX = React.useRef(new Animated.Value(0)).current;
+    const marketTabScrollViewRef = React.useRef();
+
+    const onMarketTabPress = React.useCallback(marketTabIndex => {
+        marketTabScrollViewRef?.current?.scrollToOffset({
+            offset: marketTabIndex * SIZES.width
+        })
+    })
+
     React.useEffect(() => {
         getCoinMarket()
     }, [])
@@ -78,7 +139,10 @@ const Market = ({ getCoinMarket, coins }) => {
                     backgroundColor: COLORS.gray
                 }}
             >
-                <Tabs />
+                <Tabs
+                    scrollX={scrollX}
+                    onMarketTabPress={onMarketTabPress}
+                />
             </View>
         )
     }
@@ -116,6 +180,7 @@ const Market = ({ getCoinMarket, coins }) => {
     function renderList() {
         return (
             <Animated.FlatList
+                ref={marketTabScrollViewRef}
                 data={marketTabs}
                 contentContainerStyle={{
                     marginTop: SIZES.padding
@@ -213,7 +278,40 @@ const Market = ({ getCoinMarket, coins }) => {
                                                 <Text style={{
                                                     color: COLORS.white,
                                                     ...FONTS.h4
-                                                }}>$ {item.current_price}</Text>
+                                                }}>
+                                                    $ {item.current_price}
+                                                </Text>
+
+                                                <View
+                                                    style={{
+                                                        flexDirection: 'row',
+                                                        justifyContent: "flex-end",
+                                                        alignItems: "center"
+                                                    }}
+                                                >
+                                                    {
+                                                        item.price_change_percentage_7d_in_currency != 0 &&
+                                                        <Image
+                                                            source={icons.upArrow}
+                                                            style={{
+                                                                height: 10,
+                                                                width: 10,
+                                                                tintColor: priceColor,
+                                                                transform: item.price_change_percentage_7d_in_currency > 0 ? [{ rotate: '45deg' }] : [{ rotate: '125deg'}]
+                                                            }}
+                                                        />
+                                                    }
+
+                                                    <Text
+                                                        style={{
+                                                            marginLeft: 5,
+                                                            color: priceColor,
+                                                            ...FONTS.body5
+                                                        }}
+                                                    >
+                                                    {item.price_change_percentage_7d_in_currency.toFixed(2)}%
+                                                    </Text>
+                                                </View>
                                             </View>
                                         </View>
                                     )
